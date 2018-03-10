42f3a1e448220b206732e2c1cf880c5ba3d41459
shirakaba <14055146+shirakaba@users.noreply.github.com>
2018-03-10 22:46:47 +0000

    evt.nativeEvent logged

--- App.tsx
+++ App.tsx
@@ -10,22 +10,20 @@ export default class App extends React.Component {
 
     render() {
         return (
-            <TouchableWithoutFeedback
-                onPress={
-                    (evt: GestureResponderEvent) => {
-
-                        console.log("onPress()");
-                        console.log(evt.nativeEvent);
-                    }
-                }
+            <View
+                style={styles.container}
+                onStartShouldSetResponder={(ev: GestureResponderEvent) => true}
+                onMoveShouldSetResponder={(ev: GestureResponderEvent) => true}
+                onResponderGrant={(ev: GestureResponderEvent) => { console.log(`onResponderGrant():`, ev.nativeEvent); }}
+                onResponderReject={(ev: GestureResponderEvent) => { console.log(`onResponderReject():`, ev.nativeEvent); }}
+                onResponderMove={(ev: GestureResponderEvent) => { console.log(`onResponderMove():`, ev.nativeEvent); }}
+                onResponderRelease={(ev: GestureResponderEvent) => { console.log(`onResponderRelease():`, ev.nativeEvent); }}
+                onResponderTerminationRequest={(ev: GestureResponderEvent) => true}
+                onResponderTerminate={(ev: GestureResponderEvent) => { console.log(`onResponderTerminate():`, ev.nativeEvent); }}
             >
-                <View
-                    style={styles.container}
-                >
-                    <Box size={200} colour={"red"} initialLeft={90} initialTop={75}/>
-                    <Box size={50} colour={"blue"} initialLeft={50} initialTop={100}/>
-                </View>
-            </TouchableWithoutFeedback>
+                <Box size={200} colour={"red"} initialLeft={90} initialTop={75}/>
+                <Box size={50} colour={"blue"} initialLeft={50} initialTop={100}/>
+            </View>
         );
     }
 }
