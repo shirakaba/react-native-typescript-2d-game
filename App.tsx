193211c1636841ffe8dd38d7eb3aad394912a16d
shirakaba <14055146+shirakaba@users.noreply.github.com>
2018-03-10 22:44:40 +0000

    Logging of touch events working now; just that console.log() can't cope with logging the evt itself.

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Alert, StyleSheet, Text, TouchableWithoutFeedback, View} from 'react-native';
+import {Alert, GestureResponderEvent, StyleSheet, Text, TouchableWithoutFeedback, View} from 'react-native';
 import {Box} from "./src/Box";
 
 export default class App extends React.Component {
@@ -10,20 +10,22 @@ export default class App extends React.Component {
 
     render() {
         return (
-            <View
-                style={styles.container}
-                onStartShouldSetResponder={(ev) => true}
-                onMoveShouldSetResponder={(ev) => true}
-                onResponderGrant={(evt) => { console.log(`onResponderGrant():`); }}
-                onResponderReject={(evt) => { console.log(`onResponderReject():`); }}
-                onResponderMove={(evt) => { console.log(`onResponderMove():`); }}
-                onResponderRelease={(evt) => { console.log(`onResponderRelease():`); }}
-                onResponderTerminationRequest={(evt) => true}
-                onResponderTerminate={(evt) => { console.log(`onResponderTerminate():`); }}
+            <TouchableWithoutFeedback
+                onPress={
+                    (evt: GestureResponderEvent) => {
+
+                        console.log("onPress()");
+                        console.log(evt.nativeEvent);
+                    }
+                }
             >
-                <Box size={200} colour={"red"} initialLeft={90} initialTop={75}/>
-                <Box size={50} colour={"blue"} initialLeft={50} initialTop={100}/>
-            </View>
+                <View
+                    style={styles.container}
+                >
+                    <Box size={200} colour={"red"} initialLeft={90} initialTop={75}/>
+                    <Box size={50} colour={"blue"} initialLeft={50} initialTop={100}/>
+                </View>
+            </TouchableWithoutFeedback>
         );
     }
 }
