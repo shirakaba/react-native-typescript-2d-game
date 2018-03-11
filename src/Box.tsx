7d7c28c9c2f71fbfe2eea65d21618a681b90c41d
shirakaba <14055146+shirakaba@users.noreply.github.com>
2018-03-11 18:23:25 +0000

    Half working

--- src/Box.tsx
+++ src/Box.tsx
@@ -26,10 +26,11 @@ interface State {
 }
 
 export class Box extends Component<Props, State> {
-    private maxAdvancePerTick: number = 10;
+    private timerID: number;
 
     constructor(props: Props) {
         super(props);
+        // console.log("RECONSTRUCTED");
 
         this.state = {
             speed: 10,
@@ -38,30 +39,72 @@ export class Box extends Component<Props, State> {
             left: this.props.targetLeft,
             top: this.props.targetTop
         };
+
+        this.advance = this.advance.bind(this);
+
+        // setInterval()
+        // this.advance(this.props.targetLeft, this.props.targetTop);
+    }
+
+    componentWillReceiveProps(nextProps: Props): void {
+        // Guard against un-needed renders.
+        if(nextProps.targetLeft !== this.state.left || nextProps.targetTop !== this.state.top){
+            // this.advance();
+            clearInterval(this.timerID);
+            this.timerID = window.setInterval(
+                () => this.advance(),
+                // this.props.updateFreq || 1000
+                100
+            );
+        } else {
+            clearInterval(this.timerID);
+        }
+    }
+
+    componentDidMount(): void {
+        if(this.props.colour !== "red") {
+        }
+    }
+
+    componentWillUnmount(): void {
+        clearInterval(this.timerID);
     }
 
     // rotateToPoint(newLeft: number, newTop: number): void {
     //     // const leftDiff: number = newLeft - this.state.targetLeft
     // }
 
-    advance(newLeft: number, newTop: number): void {
-        const xDiff: number = newLeft - this.state.left;
-        const yDiff: number = newTop - this.state.top;
+    advance(): void {
+        console.log(`[advance()] targetLeft: ${this.props.targetLeft.toFixed(0)}; targetTop: ${this.props.targetTop.toFixed(0)}; left: ${this.state.left.toFixed(0)}; top: ${this.state.top.toFixed(0)}`);
+        const xDiff: number = this.props.targetLeft - this.state.left;
+        const yDiff: number = this.props.targetTop - this.state.top;
 
-        const angle: number = Math.atan2(yDiff, xDiff);
-        const maxAdvanceX: number = Math.sin(angle) * this.state.speed;
+        let angle: number = Math.atan2(yDiff, xDiff);
+        const maxAdvanceX: number = Math.sin(angle + Math.sin(Math.asin(1))) * this.state.speed;
         const maxAdvanceY: number = Math.sin(angle) * this.state.speed;
+        console.log(`[advance()] angle: ${(angle * (180/3.14159)).toFixed(2)}º; maxAdvanceY: ${maxAdvanceX.toFixed(0)}; maxAdvanceX ${maxAdvanceX.toFixed(0)}`);
 
-        this.setState({
-            rotation: angle,
-            left: maxAdvanceX,
-            top: maxAdvanceY
+        this.setState((prevState: Readonly<State>, props: Props) => {
+
+            return {
+                rotation: angle,
+                left: Math.min(prevState.left + maxAdvanceX, props.targetLeft),
+                top: Math.min(prevState.top + maxAdvanceY, props.targetTop)
+            }
         });
+
+        // this.setState({
+        //     rotation: angle,
+        //     left: maxAdvanceX,
+        //     top: maxAdvanceY
+        // });
     }
 
     render() {
         const combinedStyles: Partial<ComponentStyle> = {
             backgroundColor: this.props.colour,
+            // left: this.props.targetLeft,
+            // top: this.props.targetTop,
             left: this.state.left,
             top: this.state.top,
             width: this.state.size,
